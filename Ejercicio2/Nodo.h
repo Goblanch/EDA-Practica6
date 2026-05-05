5c621ac1003a7c5527e3440deb81cc242b8ae1ae
Gonzalo Blanch Dominguez <gonzaloblanchdev@gmail.com>
2026-05-05 10:54:14 +0200

    Implementar nodo genérico

---
+++ Ejercicio2/Nodo.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <memory>
+
+template <typename T>
+class Nodo {
+private:
+    T dato;
+    std::shared_ptr<Nodo<T>> siguiente;
+
+public:
+    explicit Nodo(const T& dato) : dato(dato), siguiente(nullptr) {}
+
+    T getDato() const {return dato;}
+    std::shared_ptr<Nodo<T>> getSiguiente() const {return siguiente;}
+
+    void setDato(const T& d) {dato = d;}
+    void setSiguiente(const std::shared_ptr<Nodo<T>>& s) {siguiente = s;}
+};
